8ce54ebc67eb4615b2bff03e627d5665b8733b98
jephersonSS <jephersonsilva32@gmail.com>
2026-09-02 18:49:11 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,14 @@
+
+public class Exercicio1 {
+ public static void main(String[] args) {
+
+    int ra = 06334562;
+    String curso = "Análise e Desenvolvimento de Sistemas";
+    String nome = "Jepherson Silva de Sousa";
+
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Meu r.a é: " + ra);
+        System.out.println("Meu curso é: " + curso);
+
+    }
+}
