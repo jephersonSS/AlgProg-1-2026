d8f93a5f8f2d9627ababa2bd6ddfd3c6831169fa
jephersonSS <jephersonsilva32@gmail.com>
2026-09-02 19:03:59 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,15 @@
+
+public class Exercicio2 {
+ public static void main(String[] args) {
+
+    int idade = 22;
+    double altura = 1.78;
+    String presença = "Presente";
+    
+
+        System.out.println("a sua idade é: " + idade);
+        System.out.println("sua altura é: " + altura);
+        System.out.println("seu status na aula é: " + presença);
+
+    }
+}
